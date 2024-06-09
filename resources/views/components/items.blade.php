9938841c37103dc0af2606ae31498479a20f1b91
technofusion <matthew.jimoya10@gmail.com>
2024-06-09 14:18:51 -0800

    resources/views/components/items.blade.php

--- resources/views/components/items.blade.php
+++ resources/views/components/items.blade.php
@@ -1,14 +1,21 @@
                 <ul class="flex flex-col md:flex-row px-4">
-                    <li class="hover:text-sky-700">
-                        <a href="/login" class="block py-2 pr-4 pl-3">Sign in</a>
-                    </li>
-                    <li class="hover:text-sky-700">
-                        <a href="/register" class="block py-2 pr-4 pl-3">Sign up</a>
-                    </li>
+                    @auth
                     <li class="hover:text-sky-700">
                         <form action="/logout" method="post">
                             @csrf
                         <button class="block py-2 pr-4 pl-3" type="submit">Logout</button>
                         </form>
                     </li>
+
+                    @else
+
+                    <li class="hover:text-sky-700">
+                        <a href="/login" class="block py-2 pr-4 pl-3">Sign in</a>
+                    </li>
+                    <li class="hover:text-sky-700">
+                        <a href="/register" class="block py-2 pr-4 pl-3">Sign up</a>
+                    </li>
+
+                    @endauth
                 </ul>
+
